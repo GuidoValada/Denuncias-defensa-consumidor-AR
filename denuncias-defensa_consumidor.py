480dcd29b4e71e2afef79113cf3004bb05fd57ea
GuidoValada <Guido>
2020-05-14 04:09:34 -0300

    First

---
+++ denuncias-defensa_consumidor.py
@@ -0,0 +1,90 @@
+import csv
+
+def denuncias_rubro(denuncias):
+
+    rubros={}
+    listado = ""
+
+    for denuncia in denuncias:
+        rubro = '-'.join(denuncia['rubro'].split())
+        if  rubro not in rubros:
+            rubros[rubro] = 1
+        else:
+            rubros[rubro] += 1
+
+    for rubro in rubros:
+        listado += (' '.join(rubro.split('-')) + ': ' + str(rubros[rubro])+ '\n')
+
+    return listado
+
+def denuncias_rubro_motivo(denuncias):
+
+    rubros = {}
+    listado = ""
+
+    for denuncia in denuncias:
+        rubro = '-'.join(denuncia['rubro'].split())
+        motivo = denuncia['motivo_denuncia']
+        if  rubro not in rubros:
+            rubros[rubro] = {'CANTIDAD-DENUNCIAS':1}
+        else:
+            rubros[rubro]['CANTIDAD-DENUNCIAS'] += 1
+            if motivo not in rubros[rubro]:
+                rubros[rubro][motivo] = 1
+            else:
+                rubros[rubro][motivo]  += 1
+    
+    for rubro in rubros:
+        listado += (' '.join(rubro.split('-')) + '\n')
+        for motivo in rubros[rubro]:
+            listado += (' '.join(motivo.split('-')) + ': ' + str(rubros[rubro][motivo]) + '\n' )
+        listado += ('\n')
+
+    return listado
+
+
+with open ('denuncias-defensa-del-consumidor.csv',newline='',encoding="utf8") as f:
+
+    denuncias =csv.DictReader(f)
+
+
+    rubros_file = open('denuncias_rubro.txt','w')
+    rubros_file.write(denuncias_rubro(denuncias))
+    rubros_file.close()
+
+    f.close()
+with open ('denuncias-defensa-del-consumidor.csv',newline='',encoding="utf8") as f:
+
+    denuncias =csv.DictReader(f)
+
+    motivo_file = open('denuncias_motivo.txt','w')
+    motivo_file.write(denuncias_rubro_motivo(denuncias))
+    motivo_file.close()
+
+    f.close()
+
+
+    
+
+    
+
+
+    
+
+    
+
+
+
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
